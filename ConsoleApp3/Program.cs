4af39b0e6388ed2adda6abe928411731bbe4e3d8
alexa <alexa@ALEX_VADAN>
2024-10-06 15:39:27 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i, n, sum = 0;
+        int z;
+
+        Console.Write("numarul termenilor? ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nPatratele termenilor de la 1 pana la {0} sunt :", n);
+
+
+        for (i = 1; i <= n; i++)
+          Console.Write("{0} ", i * i);
+           
+       
+
+       
+        // tiparirea cuvintelor
+        StreamWriter fout = new StreamWriter("patrate.txt");
+        for (i = 1; i <= n; i++)
+        {
+            fout.WriteLine(i*i);
+        }
+        fout.Close();
+        
+
+    }
+}
